4d892090fa011240b6eaf0b5127d2edad7f4fc48
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 17:26:10 +0530

    signup and login forms up

---
+++ Frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,14 @@
+import LoginCard from "../Components/Login"
+import SignupCard from "../Components/SignupCard"
+
+
+const AuthPage = () => {
+  return (
+    <>
+        <LoginCard />
+        <SignupCard />
+    </>
+  )
+}
+
+export default AuthPage
