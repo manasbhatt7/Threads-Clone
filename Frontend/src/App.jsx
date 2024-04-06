dee38bbdd89f03477efc4ed56284dae47a25ada4
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 15:26:32 +0530

    added / route in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
 import Header from "./Components/Header"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Container maxW="620px">
         <Header />
         <Routes>
+          <Route path="/" element={<HomePage /> }/>
           <Route path="/:username" element={<UserPage /> }/>
           <Route path="/:username/post/:pid" element={<PostPage /> }/>
         </Routes>
