2ad71ab31c22a6be276cc32cdd5bd6f684901a4e
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 10:40:15 +0530

    getUserProfile backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,6 +2,19 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
+const getUserProfile = async (req, res) => {
+	const { username } = req.params;
+	try {
+		const user = await User.findOne({ username }).select("-password").select("-updatedAt");
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in getUserProfile: ", err.message);
+	}
+};
+
 const signupUser = async(req,res) => {
     try {
 		const { name, email, username, password } = req.body;
@@ -132,4 +145,4 @@ const updateUser = async (req, res) => {
 	}
 };
 
-export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser, getUserProfile };
