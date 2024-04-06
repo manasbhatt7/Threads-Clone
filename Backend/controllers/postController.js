c5ece0dd230c63a2156e17cac7ce221d9400ad60
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 13:09:47 +0530

    Delete Post backend

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -47,4 +47,23 @@ const getPost = async (req, res) => {
 	}
 };
 
-export { createPost, getPost };
+const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ message: "Post not found" });
+		}
+
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ message: "Unauthorized to delete post" });
+		}
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+};
+
+export { createPost, getPost, deletePost };
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { createPost, getPost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
