aad9b15bd1fc5b059c9a3ce182bf72773e56ab1a
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 13:54:49 +0530

    Reply Post backend

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { createPost, deletePost, getPost, likeUnlikePost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 
 export default router;
