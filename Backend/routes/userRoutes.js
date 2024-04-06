b02368ee3a90adb7ba4ec8ebda4c7cc342bb4ea1
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-06 10:21:56 +0530

    update profile backend

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { followUnFollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js";
+import { followUnFollowUser, loginUser, logoutUser, signupUser, updateUser } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+router.post("/update/:id", protectRoute, updateUser);
 
 
 
