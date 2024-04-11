3aa91b3bbc7200badc1bc76489dbf3939ab02a1e
manasbhatt7 <manasbhatt007@gmail.com>
2024-04-11 11:30:16 +0530

    postAtom added to optimize creating and deleting post in UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,11 +22,23 @@ function App() {
 				<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
         <Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
 
-          <Route path="/:username" element={<UserPage /> }/>
+        <Route
+					path='/:username'
+					element={
+						user ? (
+							<>
+								<UserPage />
+								<CreatePost />
+							</>
+						) : (
+							<UserPage />
+						)
+					}
+				/>
           <Route path="/:username/post/:pid" element={<PostPage /> }/>
         </Routes>
         {user && <LogoutButton />}
-        {user && <CreatePost />}
+        {/* {user && <CreatePost />} */}
     </Container>
   )
 }
